3168a432617edc7d7719aea956263353b8e9c889
Hinako Nakai <hinako.0113n@gmail.com>
2022-08-30 20:04:34 +0900

    a

--- techpod/ViewController.swift
+++ techpod/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var table: UITableView!
+    
+    var fileNameArray = [String]()
+    
+    
+    
+    var imageNameArray = [String]()
+    
+    var audioPlayer : AVAudioPlayer!
+    var songNameArray: [String] = ["カノン", "エリーゼのために", "G線上のアリア"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        table.dataSource = self
+        table.delegate = self
+        
+        fileNameArray = ["cannon", "elise", "aria"]
+        
+        imageNameArray = ["Pachelbel.jpg", "Beethoven.jpg", "Bach.jpg"]
     }
+    
 
 
 }
-
+extension ViewController:UITableViewDataSource,UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songNameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = songNameArray[indexPath.row]
+        
+        cell?.imageView?.image = UIImage(named: imageNameArray[indexPath.row])
+        return cell!
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let audioPath = URL(fileURLWithPath: Bundle.main.path(forResource: fileNameArray[indexPath.row], ofType: "mp3")!)
+        
+        audioPlayer = try? AVAudioPlayer(contentsOf: audioPath)
+        
+        audioPlayer.play()
+    }
+    
+    
+}
